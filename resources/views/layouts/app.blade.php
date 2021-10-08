4959647052128a4836230d14d5e6a1f213511641
hitsugayakun <43233348+aliepba@users.noreply.github.com>
2021-10-08 15:45:52 +0700

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  @include('includes.styles')
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    @include('includes.navbar')
+
+    @include('includes.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+   @yield('content-header')
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      @yield('content')
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  @include('includes.footer')
+</div>
+<!-- ./wrapper -->
+@include('includes.scripts')
+</body>
+</html>
