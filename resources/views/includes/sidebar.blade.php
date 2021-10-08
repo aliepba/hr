29e574f6667aaf24efde84a9137109236e49ae1d
hitsugayakun <43233348+aliepba@users.noreply.github.com>
2021-10-08 19:23:08 +0700

    Update sidebar.blade.php

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -99,6 +99,12 @@
                   <p>Penggajian Pegawai</p>
                 </a>
               </li>
+              <li class="nav-item">
+                <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p></p>
+                </a>
+              </li>
             </ul>
           </li>
         </ul>
